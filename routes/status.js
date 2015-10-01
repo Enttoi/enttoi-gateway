c52da96c5227fcb65ce8336240eafdca479bfc5f
Amir Sasson <amirsas@gmail.com>
2015-10-01 23:08:09 +0300

    fire queue event

--- routes/status.js
+++ routes/status.js
@@ -3,40 +3,68 @@
  */
  
 var express = require('express');
-var azure = require('azure-storage');
+var azure = require('azure');
 
 
-var STATUS_TABLE_NAME = process.env.STATUS_TABLE_NAME; 
-var STORAGE_ACCOUNT = process.env.STORAGE_ACCOUNT; 
+var STATUS_TABLE_NAME = process.env.STATUS_TABLE_NAME;
+var STORAGE_ACCOUNT = process.env.STORAGE_ACCOUNT;
 var STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;
 
 
 exports.report = function (req, res) {
-	var response = {
-		status  : 200,
-		success : 'Updated Successfully'
-	};
-	
-	//updating 
-	var tableService = azure.createTableService(STORAGE_ACCOUNT, STORAGE_ACCESS_KEY);
-	tableService.createTableIfNotExists(STATUS_TABLE_NAME, function () {
-		console.log("ToiletStatus Created..")
-	});
-	
-	var statusReport = {
-		PartitionKey : { '_': req.body.room, '$': 'Edm.Int32' },//room
-		RowKey: { '_': req.body.door, '$': 'Edm.Guid' },//door id
-		Status: { '_': req.body.status, '$': 'Edm.Int32' },//status 0:notInUse , 1:InUse
-		TimeStamp: { '_': new Date(), '$': 'Edm.DateTime' }
-	};
-	
-	tableService.insertOrReplaceEntity(STATUS_TABLE_NAME, statusReport, function (error) {
-		if (!error) {
-			console.log("entry added/updated");
-		}
-	});
-	
-	res.end(JSON.stringify(response));
+    var response = {
+        status  : 200,
+        success : 'Updated Successfully'
+    };
+    
+    //updating 
+    var tableService = azure.createTableService(STORAGE_ACCOUNT, STORAGE_ACCESS_KEY);
+    tableService.createTableIfNotExists(STATUS_TABLE_NAME, function () {
+        console.log("ToiletStatus Created..")
+    });
+    
+    var statusReport = {
+        PartitionKey : { '_': req.body.room, '$': 'Edm.Int32' },//room
+        RowKey: { '_': req.body.door, '$': 'Edm.Guid' },//door id
+        Status: { '_': req.body.status, '$': 'Edm.Int32' },//status 0:notInUse , 1:InUse
+        TimeStamp: { '_': new Date(), '$': 'Edm.DateTime' }
+    };
+     
+    
+    tableService.insertOrReplaceEntity(STATUS_TABLE_NAME, statusReport, function (error) {
+        if (!error) {
+            console.log("entry added/updated");
+        }
+    });
+    
+    require('azure').createServiceBusService('enttoi', 'CKwGw04VATdhm+TaUviuqKn4nHlpWZUJqC96SV7w/UU=');
+    
+    //Send Queue Message
+    var serviceBusService = azure.createServiceBusService();    
+    
+    serviceBusService.createQueueIfNotExists('statusQ', function (error) {
+        if (!error) {
+        // Queue exists
+        }
+    });
+    
+    var message = {
+        body: req.body,
+        customProperties: {
+            testproperty: 'TestValue'
+        }
+    };
+    serviceBusService.sendQueueMessage('statusQ', message, function (error) {
+        if (!error) {
+        // message sent
+        }
+    });
+    
+
+    
+
+    
+    res.end(JSON.stringify(response));
 
 
 
