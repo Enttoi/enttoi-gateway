abe35dcf49f56d3c58ddc5f68b0fd56bfa77e045
Jenya Y. <jenyay@gmail.com>
2015-10-13 21:52:16 +0300

    Minor refactoring

---
+++ models/sensor.js
@@ -0,0 +1,20 @@
+﻿exports.postValidationModel = {
+    'client': {
+        notEmpty: true,
+        isGuid: true
+    },
+    'sensor_type': {
+        notEmpty: true,
+        isLength: {
+            options: [2, 15]
+        }
+    },
+    'sensor_id': {
+        notEmpty: true,
+        isInt: true
+    },
+    'state': {
+        notEmpty: true,
+        isInt: true
+    }
+};
--- routes/sensor.js
+++ routes/sensor.js
@@ -1,52 +1,62 @@
 ﻿/*
  * Stores and notify sensors statuses
  */
-var util = require('util')
- 
-var express = require('express');
+var util = require('util'); 
 var azure = require('azure-storage');
-var entGen = azure.TableUtilities.entityGenerator;
+var sensorModel = require('../models/sensor');
+
+var tableUtilities = azure.TableUtilities.entityGenerator;
 
 var STORAGE_ACCOUNT = process.env.STORAGE_ACCOUNT || 'UseDevelopmentStorage=true'; 
 var STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;
-
 var TABLE_SENSORS_HISTORY = 'SensorsHistory';
 var TABLE_SENSORS_STATE = 'SensorsState';
 var TABLE_CLIENTS_STATE = 'ClientsState';
+var QUEUE_SENSORS_STATE = 'sensor-state-changed';
+var ALLOWED_CLIENTS = (process.env.ALLOWED_CLIENTS || 'e997b810-f0ae-4cde-b933-e2ed6430d2d1').split(';');
+
 var tableService = azure.createTableService(STORAGE_ACCOUNT, STORAGE_ACCESS_KEY);
+var queuesService = azure.createQueueService(STORAGE_ACCOUNT, STORAGE_ACCESS_KEY);
+
 tableService.createTableIfNotExists(TABLE_SENSORS_HISTORY, function () { });
 tableService.createTableIfNotExists(TABLE_SENSORS_STATE, function () { });
 tableService.createTableIfNotExists(TABLE_CLIENTS_STATE, function () { });
-
-var QUEUE_SENSORS_STATE = 'sensor-state-changed';
-var queuesService = azure.createQueueService(STORAGE_ACCOUNT, STORAGE_ACCESS_KEY);
 queuesService.createQueueIfNotExists(QUEUE_SENSORS_STATE, function () { });
 
-var ALLOWED_CLIENTS = (process.env.ALLOWED_CLIENTS || 'e997b810-f0ae-4cde-b933-e2ed6430d2d1').split(';');
+var storeHistory = function (now, body) {
+    var historyEntry = {
+        PartitionKey : tableUtilities.String(util.format('%s_%s_%s', body.client, body.sensor_type, body.sensor_id)),
+        RowKey: tableUtilities.String(now.getTime() + ''), // milliseconds since 1 January 1970 00:00:00 UTC
+        State: tableUtilities.Int32(body.state),
+        TimeStamp: tableUtilities.DateTime(now)
+    };
+    
+    // requests that arrives at the same time will override because of the RowKey
+    tableService.insertOrReplaceEntity(TABLE_SENSORS_HISTORY, historyEntry, function (error, result, response) {
+        if (error) {
+            console.log(util.format('Failed to insert/update into %s row %s due to %s.', 
+                TABLE_SENSORS_HISTORY, util.inspect(historyEntry), util.inspect(error)));
+        }
+    });
+};
+
+var storeClientAlive = function (now, body) {
+    var clientState = {
+        PartitionKey : tableUtilities.String(body.client),
+        RowKey: tableUtilities.String('LastPing'),
+        TimeStamp: tableUtilities.DateTime(now)
+    };
+    tableService.insertOrReplaceEntity(TABLE_CLIENTS_STATE, clientState, function (error, result, response) {
+        if (error) {
+            console.log(util.format('Failed to insert/update into %s row %s due to %s.',
+                 TABLE_CLIENTS_STATE, util.inspect(clientState), util.inspect(error)));
+        }
+    });
+};
 
 exports.post = function (req, res) {
     // validate request
-    req.checkBody({
-        'client': {
-            notEmpty: true,
-            isGuid: true
-        },
-        'sensor_type': {
-            notEmpty: true,
-            isLength: {
-                options: [2, 15] 
-            }
-        },
-        'sensor_id': {
-            notEmpty: true,
-            isInt: true
-        },
-        'state': {
-            notEmpty: true,
-            isInt: true
-        }
-    });
-        
+    var s = req.checkBody(sensorModel.postValidationModel);        
     var errors = req.validationErrors();
     if (errors) {
         res.status(400).send((errors));
@@ -61,33 +71,10 @@ exports.post = function (req, res) {
     
     // store history 	    
     var now = new Date();
-	var historyEntry = {
-        PartitionKey : entGen.String(util.format('%s_%s_%s', req.body.client, req.body.sensor_type, req.body.sensor_id)),
-		RowKey: entGen.String(now.getTime() + ''), // milliseconds since 1 January 1970 00:00:00 UTC
-		State: entGen.Int32(req.body.state),
-		TimeStamp: entGen.DateTime(now)
-    };
-
-	// requests that arrives at the same time will override because of the RowKey
-	tableService.insertOrReplaceEntity(TABLE_SENSORS_HISTORY, historyEntry, function (error, result, response) {
-		if (error) {
-            console.log(util.format('Failed to insert/update into %s row %s due to %s.', 
-                TABLE_SENSORS_HISTORY, util.inspect(historyEntry), util.inspect(error)));
-		}
-    });
+    storeHistory(now, req.body);
     
     // store client's keep alive
-    var clientState = {
-        PartitionKey : entGen.String(req.body.client),
-        RowKey: entGen.String('LastPing'),
-        TimeStamp: entGen.DateTime(now)
-    };
-    tableService.insertOrReplaceEntity(TABLE_CLIENTS_STATE, clientState, function (error, result, response) {
-        if (error) {
-            console.log(util.format('Failed to insert/update into %s row %s due to %s.',
-                 TABLE_CLIENTS_STATE, util.inspect(historyEntry), util.inspect(error)));
-        }
-    });
+    storeClientAlive(now, req.body);
     
     // query for sensor's current state
     var sensorStateRowKey = util.format('%s_%s', req.body.sensor_type, req.body.sensor_id);
@@ -101,12 +88,12 @@ exports.post = function (req, res) {
             if (sensorState == null || (sensorState.State._ != req.body.state && sensorState.TimeStamp._ < now)) {
                 // store updated state
                 sensorState = {
-                    PartitionKey : entGen.String(req.body.client),
-                    RowKey: entGen.String(sensorStateRowKey),
-                    State: entGen.Int32(req.body.state),
-                    TimeStamp: entGen.DateTime(now),
-                    PreviousState: entGen.Int32(sensorState ? sensorState.State._ : -1),
-                    PreviousStateDurationMs: entGen.Int32(sensorState ? Math.abs(now - sensorState.TimeStamp._) : 0)
+                    PartitionKey : tableUtilities.String(req.body.client),
+                    RowKey: tableUtilities.String(sensorStateRowKey),
+                    State: tableUtilities.Int32(req.body.state),
+                    TimeStamp: tableUtilities.DateTime(now),
+                    PreviousState: tableUtilities.Int32(sensorState ? sensorState.State._ : -1),
+                    PreviousStateDurationMs: tableUtilities.Int32(sensorState ? Math.abs(now - sensorState.TimeStamp._) : 0)
                 };
 
                 console.log(util.format('Changing state of: %s', util.inspect(req.body)));
